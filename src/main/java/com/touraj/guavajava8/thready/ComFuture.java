c6b3ffc3b94596c77f884d9c73d15d79b11e148f
touraj.ebrahimi <touraj.e58@gmail.com>
2019-01-22 16:45:16 +0100

    Completeable Future added

--- src/main/java/com/touraj/guavajava8/thready/ComFuture.java
+++ src/main/java/com/touraj/guavajava8/thready/ComFuture.java
@@ -1,4 +1,40 @@
 package com.touraj.guavajava8.thready;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
 public class ComFuture {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        System.out.println("Completable Future ....");
+        System.out.println("Completable Future ....");
+        System.out.println("Completable Future ....");
+        System.out.println("Completable Future ....");
+
+        CompletableFuture<String> future
+                = CompletableFuture.supplyAsync(() -> "MongoDB");
+
+        CompletableFuture<Void> futureRun
+                = CompletableFuture.runAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("running");}).thenRun(() -> System.out.println("Process Done!"));
+
+        System.out.println("Before Run Async");
+
+//        futureRun.get();
+
+        System.out.println("After Run Async");
+
+        System.out.println(future.get());
+
+        TimeUnit.SECONDS.sleep(10);
+
+    }
+
 }
